fc2d3681cb053d3a797504b394bab3fc612b46d5
Sergio Pacheco Pérez <sergio@MacBook-Pro-de-Sergio.local>
2024-07-03 18:46:26 -0400

    First commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+console.log('hola')
+console.log('hidiabdiaid')
+
+let edad = 18;
+let nombre = 'Juan';
+let apellido = 'Perez';
+const num = 232;
+const auth= true;
+
+const Naci:number = 86372;
+
+let frutas : (string | number)[] = ['manzama', 23, 'pollo'];
+frutas[0]= 'pollleteee';
+
+console.log(frutas);
